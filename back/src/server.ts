02cb79aa2829e874c086a28f3c5eeeddb970458f
Geronimo Kramar <gerokramar@gmail.com>
2024-10-08 06:44:53 -0300

    Update server.ts

--- back/src/server.ts
+++ back/src/server.ts
@@ -4,8 +4,11 @@ import morgan from "morgan";
 import cors from "cors"
 
 const server = express();
-
-server.use(cors())
+server.use(cors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 server.use(morgan('dev'))
 server.use(express.json())
 server.use(router)
